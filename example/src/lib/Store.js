5728342c896406abe645d3f8f8aaa40e2cf7e7af
Shiranka Miskin <shiranka.miskin@gmail.com>
2015-12-28 23:51:58 -0500

    Added redux-flux-store

--- example/src/lib/Store.js
+++ example/src/lib/Store.js
@@ -1,115 +1,30 @@
-import _ from 'lodash';
-import {createStore, applyMiddleware, compose} from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
-import DevTools from 'DevTools.jsx';
+import { fluxEnhancer } from 'redux-flux-store';
+
 import AppStore from 'stores/AppStore';
 
-/*
-   This class stores the main Redux Store and provides a wrapper
-   class to it.  All app state is stored here, and each immediate
-   property can be bound to a Store class.  An example of this
-   would be if all user data was stored in a users property.
-   The getStoreMap() function would then be:
+let logger = createLogger({
+  level: 'info',
+  duration: true
+});
 
-    getStoreMap() {
-      return {
-        users: UserStore.getUsers()
-      };
-    }
+let finalCreateStore = compose(
+  fluxEnhancer({
+    app: AppStore
+  }),
+  applyMiddleware(thunk, logger)
+);
 
- */
-
-let stateStoreMap = {
-  app: AppStore
-};
-
-class Store {
-  constructor() {
-    this.initializeStoreMap();
-    this.partiallyReducedState = null;
-    let logger = createLogger({
-      level: 'info',
-      duration: true,
-      actionTransformer: (action) => _.assign({}, action, {type: action.type.toString()})
-    });
-
-    let middleware = [thunk];
-    if (true) {// dev
-      middleware.push(logger);
-      middleware.push(require('redux-immutable-state-invariant')());
-    }
-
-    let finalCreateStore = compose(
-      applyMiddleware(thunk, logger),
-      DevTools.instrument()
-    )(createStore);
-    this.store = finalCreateStore(this.reducer.bind(this));
-  }
-
-  initializeStoreMap() {
-    let invertedMap = _.transform(stateStoreMap, (map, store, prop) => {
-      return map.set(store, prop);
-    }, new Map());
-
-    let waitingSet = new Set();
-    this.storeMap = new Map();
-    _.forEach(stateStoreMap, (store) => waitingSet.add(store));
-
-    let assignStore = (store) => {
-      if (this.storeMap.has(store)) return;
-      if (store === undefined || !waitingSet.has(store)) {
-        throw new Error('Undefined store, check for circular dependencies in storeDependencies');
-      }
-
-      waitingSet.delete(store);
-      if (Array.isArray(store.storeDependencies)) {
-        store.storeDependencies.forEach((dependency) => assignStore(dependency));
-      }
-      this.storeMap.set(store, invertedMap.get(store));
-    };
-
-    while (waitingSet.size > 0) {
-      let store = waitingSet.entries().next().value[0];
-      assignStore(store);
-    }
-  }
-
-  reduceFromStores(state, action) {
-    let newState = {};
-    this.partiallyReducedState = _.assign({}, state);
-
-    for (let entry of this.storeMap) {
-      let key = entry[1];
-      let store = entry[0];
-
-      if (!store.reduce) throw new Error(`${store.constructor.name} must provide a reduce function`);
-      let value = store.reduce.bind(store)(state[key], action);
-      if (!value) throw new Error(`${store.constructor.name}.reduce must provide a default value`);
-      newState[key] = value || state[key]; // Default to old value
-      this.partiallyReducedState[key] = newState[key];
-    }
-    this.partiallyReducedState = null;
-
-    return newState;
-  }
-
-
-  getState() {
-    return this.partiallyReducedState || this.store.getState();
-  }
-
-  subscribe(listener) {
-    return this.store.subscribe(listener);
-  }
-
-  dispatch(action) {
-    this.store.dispatch(action);
-  }
-
-  reducer(state = {}, action) {
-    return this.reduceFromStores(state, action);
-  }
+if (process.type === 'renderer' && !process.guestInstanceId) {
+  finalCreateStore = compose(
+    finalCreateStore,
+    require('DevTools').instrument()
+  );
 }
 
-export default new Store();
+let store = finalCreateStore(createStore)(() => {});
+
+export default store;
+
--- example/src/renderer/components/App.jsx
+++ example/src/renderer/components/App.jsx
@@ -10,7 +10,7 @@ export default class App extends Component {
     return (
       <div>
         {this.props.children}
-        <Provider store={Store.store}>
+        <Provider store={Store}>
           <DevTools />
         </Provider>
       </div>
