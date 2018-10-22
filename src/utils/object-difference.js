e12aac0fe8c5f2c95584e8640c5e5015b39a9d2c
Matt Way <matt__way@hotmail.com>
2018-10-22 10:48:06 +1000

    Fix object diff (#56)
    * fixed object difference function
    
    * formatting

--- src/utils/object-difference.js
+++ src/utils/object-difference.js
@@ -19,6 +19,8 @@ module.exports = function objectDifference(old, curr) {
   const deleted = {};
 
   keys(curr).forEach(key => {
+    if (old[key] === curr[key]) return;
+
     if (isShallow(curr[key]) || isShallow(old[key])) {
       updated[key] = curr[key];
     } else {
@@ -28,8 +30,7 @@ module.exports = function objectDifference(old, curr) {
     }
   });
 
-  keys(old)
-    .forEach(key => curr[key] === undefined && (deleted[key] = true));
+  keys(old).forEach(key => curr[key] === undefined && (deleted[key] = true));
 
   return { updated, deleted };
 };
